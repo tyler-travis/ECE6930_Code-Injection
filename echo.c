9c43f8d472816d14a40593488e1518f111634aa9
Justin Cox <justin.n.cox@gmail.com>
2016-02-20 21:13:16 -0700

    Code echo.c is working

---
+++ echo.c
@@ -0,0 +1,72 @@
+//*****************************************************************************
+//
+//	Name(s): Justin Cox & Tyler Travis
+//
+//
+//
+//*****************************************************************************
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+
+void echo(void);
+
+int main(void)
+{
+
+		uint32_t port = 0;
+		uint32_t baudrate = 115200;
+		uint32_t srcCLK = 16000000;
+	
+		// Set the clocking to run directly from the crystal.
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+
+    // Enable the GPIO port that is used for the on-board LED.
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    // Enable the GPIO pins for the LED (PF2 & PF3).
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+		
+		//*********************************************
+    //    Initialize the UART.
+		//*********************************************
+	
+    // Enable the GPIO Peripheral used by the UART.
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+		// Enable UART0
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+   
+    // Configure GPIO Pins for UART mode.
+    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    
+		// Use the internal 16MHz oscillator as the UART clock source.
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+    
+    // Initialize the UART for console I/O.
+    UARTStdioConfig(port, baudrate, srcCLK);
+
+    while(1)
+    {
+        echo();
+    }
+}
+
+void echo(void){
+	
+	char buffer[32];
+	
+	UARTgets(buffer, sizeof(buffer));
+	
+	UARTprintf("%s \n", buffer);
+	
+}
